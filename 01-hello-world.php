51630390a31dfb74c50aafc9c3f38073e2bff985
GustavoTestoni <gustavoftestoni@gmail.com>
2024-10-15 14:44:27 -0300

    delimitadores

---
+++ 01-hello-world.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com php</title>
+</head>
+<body>
+    <h1>Experiências com php</h1>
+    <hr>
+
+    <!-- Delimitador de abertura -->
+    <?php
+
+
+
+    ?>
+</body>
+</html>
